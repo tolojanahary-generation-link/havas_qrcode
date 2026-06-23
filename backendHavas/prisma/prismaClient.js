8775ef5c6871e7271a8daa66e853723b4d02fceb
207-TOLOJANAHARY <tolojanahary@generation.link>
2026-06-23 12:09:40 +0300

    Correction de gitignore

---
+++ backendHavas/prisma/prismaClient.js
@@ -0,0 +1,16 @@
+// ============================================================
+// Prisma Client — Singleton instance
+// ============================================================
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({
+  adapter,
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+});
+
+export default prisma;
